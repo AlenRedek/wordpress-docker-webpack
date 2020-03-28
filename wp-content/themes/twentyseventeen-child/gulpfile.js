1a960c301008eb40fc416753f4fa6c6a75768d35
Alen Branko Redek <alen@redek.me>
2020-03-28 21:57:54 +0100

    Fixed gulp watch

--- wp-content/themes/twentyseventeen-child/gulpfile.js
+++ wp-content/themes/twentyseventeen-child/gulpfile.js
@@ -7,9 +7,9 @@ function watch() {
 		"port": 8001,
 	});
 
-	gulp.watch('./*.php').on('change',browserSync.reload);
-	gulp.watch('./*.js').on('change', browserSync.reload);
-	gulp.watch('./*.css').on('change', browserSync.reload);
+	gulp.watch('./**/*.php').on('change', browserSync.reload);
+	gulp.watch('./**/*.js').on('change', browserSync.reload);
+	gulp.watch('./**/*.css').on('change', browserSync.reload);
 }
 
 exports.default = watch;
