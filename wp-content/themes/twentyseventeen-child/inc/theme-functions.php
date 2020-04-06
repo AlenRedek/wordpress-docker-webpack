38b533b2d20f0c9ed1f7e7e8f74f05cd62c8f05a
Alen Branko Redek <alen@redek.me>
2020-04-06 19:26:16 +0200

    Removed namespace

--- wp-content/themes/twentyseventeen-child/inc/theme-functions.php
+++ wp-content/themes/twentyseventeen-child/inc/theme-functions.php
@@ -1,13 +1,11 @@
 <?php
 
-namespace ABR\Theme;
-
-function is_dev() {
+function abr_is_dev() {
   return current_user_can('administrator');
 }
 
 function avar_dump() {
-  if (is_dev()) {
+  if (abr_is_dev()) {
     $args = func_get_args();
     echo '<pre>';
     var_dump($args);
