ed8b211f5475796ec25fff0bdf9f27facb3b7f13
Alen Branko Redek <alen@redek.me>
2020-05-01 18:13:21 +0200

    Removed Gulp and Browsersync

--- wp-content/themes/twentyseventeen-child/assets/js/main.js
+++
@@ -1,5 +0,0 @@
-jQuery.noConflict(); // Reverts '$' variable back to other JS libraries
-
-jQuery(document).ready(function($) {
-	 // code using $ as alias to jQuery
-});
--- wp-content/themes/twentyseventeen-child/inc/init.php
+++ wp-content/themes/twentyseventeen-child/inc/init.php
@@ -7,7 +7,6 @@ function abr_theme_setup() {
 
 add_action('wp_enqueue_scripts', 'abr_theme_scripts');
 function abr_theme_scripts() {
-  wp_enqueue_script('theme-main', get_stylesheet_directory_uri() . '/assets/js/main.js', ['jquery'], false, true);
 
   $parent_style = 'parent-style';
   wp_enqueue_style($parent_style, get_template_directory_uri() . '/style.css');
