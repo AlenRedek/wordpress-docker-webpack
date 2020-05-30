f8a75f6bb8bba47fb3b2804d1910131c3082756c
Alen Branko Redek <alen@redek.me>
2020-05-30 17:24:26 +0200

    Replaces LiveReload with BrowserSync

--- wp-content/themes/twentyseventeen-child/webpack.config.js
+++ wp-content/themes/twentyseventeen-child/webpack.config.js
@@ -1,6 +1,9 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+const LiveReloadPlugin = require('webpack-livereload-plugin');
 const defaultConfig = require('@wordpress/scripts/config/webpack.config');
+const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
   ...defaultConfig,
@@ -38,9 +41,21 @@ module.exports = {
     ],
   },
   plugins: [
-    ...defaultConfig.plugins,
+    // Replace LiveReload with BrowserSync in order to watch PHP files
+    ...defaultConfig.plugins.filter(
+      (plugin) => !(plugin instanceof LiveReloadPlugin),
+    ),
+    !isProduction &&
+      new BrowserSyncPlugin(
+        {
+          proxy: 'http://localhost:8000',
+          files: ['**/*.php'],
+        },
+        // Prevent BrowserSync from reloading the page and let Webpack do it
+        { reload: false },
+      ),
     new MiniCssExtractPlugin({
       filename: '[name]/[name].css',
     }),
-  ],
+  ].filter(Boolean),
 };
