e7df6df2192f57b7dc67809d3f4f1c1ffcb23ebb
Alen Branko Redek <alen@redek.me>
2020-12-20 18:30:14 +0100

    Added global methods format_datetime and get_current_language

--- wp-content/themes/redex/inc/theme-functions.php
+++ wp-content/themes/redex/inc/theme-functions.php
@@ -30,3 +30,31 @@ function rdx_dump() {
 	// phpcs:ignore
 	echo '<hr><pre>', var_dump( $output ), '</pre><hr>';
 }
+
+/**
+ * Return formatted datetime based on WP settings
+ *
+ * @param string $datetime Standard format Y-m-d.
+ *
+ * @return string
+ */
+function rdx_format_datetime( $datetime ) {
+	$format = sprintf(
+		'%s %s',
+		get_option( 'date_format' ),
+		get_option( 'time_format' )
+	);
+
+	return date_i18n( $format, strtotime( $datetime ) );
+}
+
+/**
+ * Return current language ISO code (en, it, etc)
+ *
+ * @return string
+ */
+function rdx_get_current_language() {
+	list($language_code) = explode( '_', get_locale() );
+
+	return $language_code;
+}
